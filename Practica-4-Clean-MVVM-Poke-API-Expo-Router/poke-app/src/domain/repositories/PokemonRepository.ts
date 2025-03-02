ead7b6cf5d0b9f59d68cbd3b8ad9b66467722234
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2025-03-01 21:42:20 -0600

    Update PokemonRepository.ts

--- Practica-4-Clean-MVVM-Poke-API-Expo-Router/poke-app/src/domain/repositories/PokemonRepository.ts
+++ Practica-4-Clean-MVVM-Poke-API-Expo-Router/poke-app/src/domain/repositories/PokemonRepository.ts
@@ -3,4 +3,5 @@ import Pokemon from "../entities/Pokemon";
 export interface PokemonRepository {
     GetPokemons(limit: number): Promise<Pokemon[] | []>
     GetPokemonDescription(idPokemon: string, language: string): Promise<string>
+    GetPokemonsFetched(): Promise<Pokemon[] | []>
 }
