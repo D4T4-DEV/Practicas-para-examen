38814d65a73f861cf6e44e6c2168a300cce2c663
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2025-03-03 12:05:26 -0600

    Update _layout.tsx

--- Practica-4-Clean-MVVM-Poke-API-Expo-Router/poke-app/app/_layout.tsx
+++ Practica-4-Clean-MVVM-Poke-API-Expo-Router/poke-app/app/_layout.tsx
@@ -2,11 +2,12 @@ import StackLayout from "../src/presentation/navigation_layouts/StackLayout";
 
 import React from 'react'
 import PokemonProvider from "../src/presentation/viewmodels/PokemonViewModel";
-import TabsLayout from "../src/presentation/navigation_layouts/TabsLayout";
+import { StatusBar } from "expo-status-bar";
 
 const _layout = () => {
     return (
         <PokemonProvider>
+            <StatusBar style="auto" />
             <StackLayout />
         </PokemonProvider>
     )
